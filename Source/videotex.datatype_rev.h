9226e6957cdb6036e910e028293fdeda0407c8e2
Chris Young <chris@unsatisfactorysoftware.co.uk>
2020-06-28 20:33:11 +0100

    Update version and documentation for release

--- Source/videotex.datatype_rev.h
+++ Source/videotex.datatype_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
 #define REVISION	6
-#define DATE		"16.6.2020"
+#define DATE		"28.6.2020"
 #define VERS		"videotex.datatype 1.6"
-#define VSTRING		"videotex.datatype 1.6 (16.6.2020)\r\n"
-#define VERSTAG		"\0$VER: videotex.datatype 1.6 (16.6.2020)"
+#define VSTRING		"videotex.datatype 1.6 (28.6.2020)\r\n"
+#define VERSTAG		"\0$VER: videotex.datatype 1.6 (28.6.2020)"
